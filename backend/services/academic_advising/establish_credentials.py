18a5c7dedf26cd249d43ec229a439ce56795c468
Hope <hopha@email.unc.edu>
2024-12-08 01:38:40 -0500

    Update establish_credentials.py

--- backend/services/academic_advising/establish_credentials.py
+++ backend/services/academic_advising/establish_credentials.py
@@ -1,6 +1,7 @@
 """Establishes the source for the google api service account credentials. This will need to be edited manually depending on if the branch is in development or deployment."""
 
 import json
+from backend.env import getenv
 
 __authors__ = ["Hope Fauble"]
 
@@ -8,12 +9,12 @@ def getcredentials():
 
     ## During testing (outside of stage branch) bring the credentials .json to the root directory, and make sure it is included in the .gitignore
     
-    with open("csxl-academic-advising-feature.json") as file:
-        creds = json.load(file)
+    # with open("csxl-academic-advising-feature.json") as file:
+    #     creds = json.load(file)
 
     ## For deployment (on stage branch) establish the .json as an environmental variable in the cloudapps deployment and retrieve the credentials from the environement.
     
-    # creds = getenv("GOOGLE_CREDS")
-    # json.load(creds)
+    creds = getenv("GOOGLE_CREDS")
+    json.load(creds)
 
     return creds
